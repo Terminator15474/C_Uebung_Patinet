c41a10173813cb32f87624ab86a86f1fba1dd413
Terminator15474 <jandommert8@gmail.com>
2023-01-31 09:49:39 +0100

    some ui stuff

---
+++ src/main/java/com/example/c_uebung_patinet/MainView.java
@@ -0,0 +1,26 @@
+package com.example.c_uebung_patinet;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class MainView {
+    @FXML
+    private ListView<?> lv_patienten;
+
+    @FXML
+    void addPatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changePatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePatient(ActionEvent event) {
+
+    }
+
+}
