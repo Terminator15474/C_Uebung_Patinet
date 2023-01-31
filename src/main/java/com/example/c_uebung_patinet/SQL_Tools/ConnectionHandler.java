b71f78cdea2ea487dc5345ac068f3797201e2b01
thselukas <lukimayer12345@gmail.com>
2023-01-31 09:18:47 +0100

    more database stuff

--- src/main/java/com/example/c_uebung_patinet/SQL_Tools/ConnectionHandler.java
+++ src/main/java/com/example/c_uebung_patinet/SQL_Tools/ConnectionHandler.java
@@ -1,4 +1,17 @@
 package com.example.c_uebung_patinet.SQL_Tools;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionHandler {
+    Connection c = null;
+    public void initDatabaseConnection() throws SQLException {
+        c = DriverManager.getConnection("jdbc:derby://localhost:1527/Database", "Database", "Database");
+    }
+
+    public void closeDatabaseConnection() throws SQLException {
+        c.close();
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.c_uebung_patinet {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.c_uebung_patinet to javafx.fxml;
