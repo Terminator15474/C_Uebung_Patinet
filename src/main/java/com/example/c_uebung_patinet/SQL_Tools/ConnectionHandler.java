5a4ffca810b6df1af50fdec212c442b43293fc10
Alexander <xandlholzi@gmail.com>
2023-01-31 18:54:06 +0100

    selectPatientId added

--- src/main/java/com/example/c_uebung_patinet/SQL_Tools/ConnectionHandler.java
+++ src/main/java/com/example/c_uebung_patinet/SQL_Tools/ConnectionHandler.java
@@ -3,6 +3,7 @@ package com.example.c_uebung_patinet.SQL_Tools;
 
 import com.example.c_uebung_patinet.SQL_Model.Land;
 import com.example.c_uebung_patinet.SQL_Model.Patient;
+import com.example.c_uebung_patinet.SQL_Model.Religion;
 
 import java.sql.*;
 
@@ -16,14 +17,7 @@ public class ConnectionHandler {
         c.close();
     }
 
-    /**
-     *
-     * TODO: Add a check if the patient already exists, then modify them in the database
-     * @param p Patient to be added to Database
-     * @return the number of lines effected in the Database
-     * @throws Exception when an SQLException occurs or the connection with the database is not initialised
-     */
-    public int insertPatientInDatabase(Patient p) throws Exception {
+    public void insertPatientInDatabase(Patient p) throws Exception {
         if(c == null) {
             throw new Exception("Connection is not initialized");
         }
@@ -33,34 +27,38 @@ public class ConnectionHandler {
         ps.setInt(1, p.getSvnr());
         ps.setString(2, p.getVorname());
         ps.setString(3, p.getNachname());
-        ps.setString(4, p.getGeburtsname());
-        ps.setString(5, p.getTitle());
-        ps.setString(6, p.getNamenszuatz());
-        ps.setDate(7, (Date)p.getGeburtsdatum());
-        ps.setString(8, p.getGeschlecht());
-        ps.setString(9, p.getFamilienstand());
-        ps.setString(10, p.getStaatsangehörigkeit().getKuerzel());
-        ps.setString(11, p.getPostleitzahl());
-        ps.setString(12, p.getHausnr());
-        ps.setString(13, p.getTel());
-        ps.setInt(14, p.getReligionszugehörigkeit().getId());
-        return ps.executeUpdate();
-    }
+        ps.setString(4, p.getTitle());
+        ps.setString(5, p.getNamenszuatz());
 
-    /**
-     * TODO: add a check if the country already exists and if it is already there do not execute
-     * @param l The country to be inserted into the database
-     * @return the number of lines effected in the database
-     * @throws Exception when an SQLException occurs or the connection with the database is not initialised
-     */
-    public int insertCountryIntoDatabase(Land l) throws Exception {
-        if(c == null) {
-            throw  new Exception("Connection is not initialized");
-        }
-        PreparedStatement ps = c.prepareStatement("INSERT INTO DATABASE.LAND VALUES(?, ?, ?)");
-        ps.setString(1, l.getKuerzel());
-        ps.setString(2, l.getName());
-        ps.setString(3, l.getVorwahl());
-        return ps.executeUpdate();
+        public void selectPatientId (int svnr) throws SQLException
+        {
+            PreparedStatement prs = c.prepareStatement("SELECT * from Patient where id = svnr");
+            ResultSet rs = prs.executeQuery();
+            Patient pat = new Patient();
+            while (rs.next()){
+                pat.setSvnr(rs.getInt(1));
+                pat.setVorname(rs.getString(2));
+                pat.setNachname(rs.getString(3));
+                pat.setGeburtsname(rs.getString(4));
+                pat.setTitle(rs.getString(5));
+                pat.setNamenszuatz(rs.getString(6));
+                pat.setGeburtsdatum(rs.getDate(7));
+                pat.setGeburtsort(rs.getString(8));
+                pat.setGeschlecht(rs.getString(9));
+                pat.setFamilienstand(rs.getString(10));
+                Land l = new Land();
+                l.setVorwahl(rs.getString(11));
+                l.setName(rs.getString(12));
+                l.setKuerzel(rs.getString(13));
+                pat.setPostleitzahl(rs.getString(14));
+                pat.setOrt(rs.getString(15));
+                pat.setStrasse(rs.getString(16));
+                pat.setHausnr(rs.getString(17));
+                pat.setTel(rs.getString(18));
+                Religion rel = new Religion();
+                rel.setId(rs.getInt(19));
+                rel.setName(rs.getString(20));
+                c.close();
+            }
     }
 }
