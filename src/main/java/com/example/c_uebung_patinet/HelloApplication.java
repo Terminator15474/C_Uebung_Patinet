d4c342a7bebac1749aa00193394134a316137886
Terminator15474 <jandommert8@gmail.com>
2023-02-01 10:25:01 +0100

    some controller stuff

--- src/main/java/com/example/c_uebung_patinet/HelloApplication.java
+++ src/main/java/com/example/c_uebung_patinet/HelloApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class  HelloApplication extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Patientenstammdatenverwaltung");
+        stage.setTitle("Patientenverwaltung");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/c_uebung_patinet/MainViewController.java
+++ src/main/java/com/example/c_uebung_patinet/MainViewController.java
@@ -1,12 +1,34 @@
 package com.example.c_uebung_patinet;
 
+import com.example.c_uebung_patinet.SQL_Model.Patient;
+import com.example.c_uebung_patinet.SQL_Tools.ConnectionHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+    ConnectionHandler connectionHandler = null;
+    static Patient selected_Patient = null;
     @FXML
-    private ListView<?> lv_patienten;
+    private ListView<Patient> lv_patienten;
+    @FXML
+    private Label label_error;
+    @FXML
+    private Button btn_change;
+    @FXML
+    private Button btn_delete;
+    @FXML
+    private Button btn_add;
 
     @FXML
     void addPatient(ActionEvent event) {
@@ -23,4 +45,25 @@ public class MainViewController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            connectionHandler = new ConnectionHandler();
+            List<Patient> patients = connectionHandler.selectPatients();
+            lv_patienten.setItems(FXCollections.observableList(patients));
+            btn_change.setDisable(true);
+            btn_delete.setDisable(true);
+            lv_patienten.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Patient>() {
+                @Override
+                public void changed(ObservableValue<? extends Patient> observableValue, Patient patient, Patient t1) {
+                    btn_change.setDisable(false);
+                    btn_delete.setDisable(false);
+                    selected_Patient = t1;
+                }
+            });
+
+        } catch (Exception e) {
+            label_error.setText("An critical error occured! Please seek help and pray!");
+        }
+    }
 }
